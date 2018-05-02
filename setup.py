077c01b992b06d993e56fd98dbc87bda35647aa0
Lara Maia <dev@lara.click>
2018-05-02 20:44:50 -0300

    setup: build translations

--- setup.py
+++ setup.py
@@ -17,8 +17,16 @@
 #
 
 import os
+from distutils.command.build_py import build_py
+from distutils.command.install import install
+from distutils.command.install_data import install_data
 from distutils.command.install_scripts import install_scripts
 from distutils.core import setup
+from importlib.machinery import SourceFileLoader
+
+po_build_path = os.path.join('build', 'share', 'locale')
+build_translations_path = os.path.join('i18n', 'build_translations.py')
+build_translations = SourceFileLoader('build_translations', build_translations_path).load_module()
 
 
 class RemoveExtension(install_scripts):
@@ -28,6 +36,37 @@ class RemoveExtension(install_scripts):
         if os.name != 'nt':
             for script in self.get_outputs():
                 os.rename(script, script[:-3])
+
+
+class BuildTranslations(build_py):
+    def run(self):
+        build_py.run(self)
+        build_translations.build(os.path.join(po_build_path))
+
+
+class InstallTranslations(install_data):
+    def run(self):
+        base_directory = 'share'
+        output_directory = os.path.join(self.install_dir, base_directory)
+
+        self.mkpath(output_directory)
+
+        for root, directories, files in os.walk(po_build_path):
+            current_folder = root[root.index(os.path.sep) + 1 + len(base_directory) + 1:]
+            self.mkpath(os.path.join(output_directory, current_folder))
+
+            for file in files:
+                output, _ = self.copy_file(
+                    os.path.join(root, file),
+                    os.path.join(output_directory, current_folder)
+                )
+                self.outfiles.append(output)
+
+
+class Install(install):
+    def run(self):
+        install.run(self)
+        self.run_command('install_data')
 
 
 setup(
@@ -42,5 +81,10 @@ setup(
     package_dir={'steam_tools_ng_ui': 'ui'},
     scripts=['steam-tools-ng.py'],
     requires=['stlib'],
-    cmdclass={'install_scripts': RemoveExtension}
+    cmdclass={
+        'build_py': BuildTranslations,
+        'install': Install,
+        'install_scripts': RemoveExtension,
+        'install_data': InstallTranslations,
+    }
 )
