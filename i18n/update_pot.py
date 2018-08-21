0bd234710e00daed58701ab1ab75b99b55b471b3
Lara Maia <dev@lara.click>
2018-08-21 18:00:14 -0300

    i18n: Update translation template

--- i18n/update_pot.py
+++ i18n/update_pot.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     output_file = os.path.join('i18n', 'steam-tools-ng.pot')
     translatable_files = []
 
-    for root, dirs, files in os.walk('steam_tools_ng'):
+    for root, dirs, files in os.walk('src'):
         for file in files:
             if file.endswith(".py"):
                 translatable_files.append(os.path.join(root, file))
