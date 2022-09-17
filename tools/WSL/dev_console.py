cee6417baa0686774f13964426bc038712956a34
ShyPixie <dev@lara.monster>
2022-09-17 16:55:38 -0300

    Fix copyright notices

--- tools/WSL/dev_console.py
+++ tools/WSL/dev_console.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 #
-# Lara Maia <dev@lara.monster> <YEAR>
+# Lara Maia <dev@lara.monster> 2015 ~ 2022
 #
-# The <program> is free software: you can redistribute it and/or
+# The Steam Tools NG is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
 #
-# The <program> is distributed in the hope that it will be useful,
+# The Steam Tools NG is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
